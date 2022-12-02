699fd23487a58a54d4c6440a0c6c8e258d146078
AmanNinave <amanninave987@gmail.com>
2022-12-02 19:54:58 +0530

    Login name improved

--- An-SkinStore/HomePage/ProductListPage.js
+++ An-SkinStore/HomePage/ProductListPage.js
@@ -1,7 +1,7 @@
 var amanninave =""
 var Wishlst = localStorage.getItem("wishlist") || [];
 
-localStorage.setItem("username" , amanninave);
+
 var userN = localStorage.getItem("username");
 
 document.querySelector("#userName").textContent = userN;
